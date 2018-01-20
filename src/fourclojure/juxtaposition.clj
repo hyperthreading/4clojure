fa052b703a5a5c6c4969d0997a58a73a4aac2c6a
hyperthreading <no1expman@gmail.com>
2018-01-20 14:29:27 +0900

    Solve two more problems

---
+++ src/fourclojure/juxtaposition.clj
@@ -0,0 +1,11 @@
+(ns fourclojure.juxtaposition
+  (:require [clojure.test :refer [run-tests with-test is]]))
+
+
+(with-test
+  (defn juxta [& fns]
+    (fn [& args]
+      (vec (map #(apply % args) fns))))
+  (is (= [21 6 1] ((juxta + max min) 2 3 5 1 6 4)))
+  (is (= ["HELLO" 5] ((juxta #(.toUpperCase %) count) "hello")))
+  (is (= [2 6 4] ((juxta :a :c :b) {:a 2, :b 4, :c 6, :d 8 :e 10}))))
---
+++ src/fourclojure/word_sorting.clj
@@ -0,0 +1,9 @@
+(ns fourclojure.word-sorting
+  (:require [clojure.test :refer [run-tests with-test is]]))
+
+(with-test
+  (defn wsort [s]
+    (vals (into {} (sort (map #(vector (.toLowerCase %) %) (re-seq #"\b\w+" s))))))
+  (is (= (wsort "Fools fall for foolish follies.")
+         ["fall" "follies" "foolish" "Fools" "for"])))
+
