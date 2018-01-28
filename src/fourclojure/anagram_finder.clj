4dc579aac2ba340e31d6defa3e77b3d518735d12
root <root@DESKTOP-QSU41HA.localdomain>
2018-01-28 13:19:37 +0900

    Solve anagram finder

---
+++ src/fourclojure/anagram_finder.clj
@@ -0,0 +1,18 @@
+(ns fourclojure.anagram-finder
+  (:require [clojure.test :refer [with-test is run-tests]]))
+
+(with-test
+  (defn anagrams
+    [v]
+    (->> v
+         (group-by sort)
+         vals
+         (filter next)
+         (map set)
+         set))
+  (is (= (anagrams ["meat" "mat" "team" "mate" "eat"])
+         #{#{"meat" "team" "mate"}})))
+
+
+
+
