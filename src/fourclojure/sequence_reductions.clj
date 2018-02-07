e890bb866e62177fecc723ddafe1fb80525ee7e9
hyperthreading <no1expman@gmail.com>
2018-02-07 11:51:49 +0900

    Solve sequence reductions

---
+++ src/fourclojure/sequence_reductions.clj
@@ -0,0 +1,17 @@
+(ns fourclojure.sequence-reductions
+  (:require [clojure.test :refer [with-test run-tests is]]))
+
+(with-test
+  (defn seq-red
+    ([f coll]
+     (drop 1 (seq-red f (f) coll)))
+    ([f default coll]
+     (lazy-seq (cons default
+                     (if coll
+                        (seq-red f
+                                 (f default
+                                    (first coll))
+                                 (next coll)))))))
+
+  (is (= (take 5 (seq-red + (range)))
+         [0 1 3 6 10])))
